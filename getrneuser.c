2bee1020b97031f40331cb2eccff24dcb6f5b414
Hrushikesh Kute <hrushi.kute@gmail.com>
2016-02-23 23:24:21 +0530

    Program to create thread and wait/Join thread

--- getrneuser.c
+++ getrneuser.c
@@ -1,11 +1,13 @@
 #include<unistd.h>
 #include<sys/types.h>
 #include <stdio.h>
+#include<stdlib.h>
 int main (int argc, char *argv[])
 {
 
+int status;
 uid_t r,e;
-
+pid_t proc_id;
 uid_t getuid(void);
 uid_t getgid(void);
 
@@ -18,6 +20,45 @@ printf("Process  Id: %d\n\n",getpid());
 
 
 printf("Process Gruop Id: %d\n\n",getpgrp());
+
+printf("process Group ID:%d\n\n",getpgid(getpid()));
+
+proc_id=fork();
+
+if(proc_id== -1)
+{
+	perror("fork:\n");
+	exit(EXIT_FAILURE);
+
+}
+else if(!proc_id)
+{
+	printf("Hi I am a Child !!! \n\n");
+	printf("CHild Process  Id: %d\n\n",getpid());
+
+
+	printf("Child Process Gruop Id: %d\n\n",getpgrp());
+
+	printf("Child process Group ID:%d\n\n",getpgid(getpid()));
+
+	status=setpgid(getpid(),5000);
+	if(status == -1 )
+	{
+		perror("setpgid:\n\n");
+		exit(EXIT_FAILURE);
+	}	
+	else
+	{
+		 printf("CHild Process  Id: %d\n\n",getpid());
+
+
+        	printf("Child Process Gruop Id: %d\n\n",getpgrp());
+	
+	        printf("Child process Group ID:%d\n\n",getpgid(getpid()));
+	}
+	
+
+}
 
 return 0;
 }
