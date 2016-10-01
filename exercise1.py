2cb161388c3b08454cd34d07e2a4962fb459a0f1
Hrushikesh Kute <hrushi.kute@gmail.com>
2016-10-01 21:00:45 +0530

    importance of single quotes and double quotes

---
+++ exercise1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+'''
+Document interlude 
+Sample program to test the double quotes and single quotes,
+while printing.
+
+Author: Hrushikesh Kute
+
+Copyright 2016.
+ 
+'''
+print "Hello World !"
+
+print "Hello Again"
+
+print "I like typing this."
+
+print "This is fun."
+print 'Yay ! Print ho raha hai.'
+
+print "I'd much rather you 'not'."
+
+print 'I "said" do not touch this.'
+
+print __doc__
